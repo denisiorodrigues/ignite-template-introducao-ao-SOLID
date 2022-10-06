4a219753e322e9f6500a0cfaebe74166b92ce518
Rodrigues <denisio.rodrigues@capgemini.com>
2022-10-06 12:31:10 -0300

    Primeiro commit (Implementação + Swagger)

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import { app } from ".";
+import swaggerUI from "swagger-ui-express";
+
+import swaggerFile  from "./swagger.json"
+
+app.use("/swagger/", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
